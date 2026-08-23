b53201992891080a8559f842ec08aac1a41e3670
ReemHisham <reemh227@gmail.com>
2026-08-24 01:02:12 +0300

    Remove mk.js from tracking

--- mk.js
+++
@@ -1,7 +0,0 @@
-<script>
-    var searchTerms = ;
-    document.write('<img src="/resources/images/tracker.gif?searchTerms=' ' " onload="alert(1)" >');
-</script>
-
-
-+encodeURIComponent(searchTerms)+
